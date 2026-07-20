495cec1467c7950c03d9aa5ac86da532168b86be
the-young-swag <xplizit.nm7@gmail.com>
2026-07-20 21:53:54 +0800

    fixed accounts.js

---
+++ User_Authentication_System/accounts.js
@@ -0,0 +1,18 @@
+const accounts = [
+    {
+        id:1,
+        username: 'admin',
+        name: 'admin',
+        email: 'admin@admin.com',
+        password: 'admin' 
+    },
+    {   id:2,
+        username: 'trevor_mundo',
+        name: 'Trevor Mundo',
+        email: 'trevor_mundo@admin.com',
+        password: 'admin'    
+    }
+
+];
+
+module.exports = accounts;
