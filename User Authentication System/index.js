56f1ca2080176388f2f58246c30b35766c085f30
the-young-swag <xplizit.nm7@gmail.com>
2026-07-20 20:49:18 +0800

    New Folder: User Authentication System
    Added: index.js

---
+++ User Authentication System/index.js
@@ -0,0 +1,25 @@
+const readline = require("readline/promises");
+const logIn = require("./logIn.js");
+
+const input = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+async function main() {
+
+    const username = await input.question("Username: ");
+    const password = await input.question("Password: ");
+
+    try {
+        const result = await logIn(username, password);
+        console.log(`Welcome, ${result.name}!`);
+
+    } catch (error) {
+        console.log(error);
+    }
+    input.close();
+}
+
+main();
