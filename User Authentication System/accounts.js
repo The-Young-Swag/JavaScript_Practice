e70d2cb0da700bc5b0d1cf7a1f623ea346997e1f
the-young-swag <xplizit.nm7@gmail.com>
2026-07-20 20:52:11 +0800

    added accounts.js

---
+++ User Authentication System/accounts.js
@@ -0,0 +1,14 @@
+[
+    {
+        id:1, 
+        name: 'admin',
+        password: 'admin' 
+    },
+    {   id:2,
+        name: 'trevor_mundo',
+        password: 'admin'    
+    }
+
+]
+
+module.exports = accounts;
