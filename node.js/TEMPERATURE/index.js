b76ba7affc2bed549eea799b3b4d258a64e3ace9
the-young-swag <xplizit.nm7@gmail.com>
2026-07-21 23:12:13 +0800

    cleaned and fixed bugs

--- node.js/TEMPERATURE/index.js
+++ node.js/TEMPERATURE/index.js
@@ -1,3 +1,5 @@
+const toCelsius = require("./toCelsius");
+const toFarenheit = require("./toFarenheit");
 const readline = require("readline/promises");
 
 const input = readline.createInterface({
@@ -12,21 +14,28 @@ async function main(){
             const tempSelect = await input.question("\nSelect 1 to convert F to C\nSelect 2 to convert C to F: ");
 
             if (tempSelect === "1"){
-                const convertToCelsius = async() => {
-                   const temp = Number(await input.question("Convert to Celsius: "));
-                   const result = (temp - 32) * (5/9);
-                   console.log(`${temp}°F to Celsius is ${result}°C`);
-                   
+                const temp = Number(await input.question("Convert to Celsius: "));
+                
+                try{
+                    const result = await toCelsius(temp);
+                    console.log(`The result is: ${result}`)
+
+                } catch (error){
+                    console.log(error)
                 }
-                await convertToCelsius();
 
             } else if (tempSelect === "2"){
-                const convertToFarenheit = async() => {
-                    const temp = Number(await input.question("Convert to Farenheit: "));
-                    const result = (temp * 9/5) + 32;
-                    console.log(`${temp}°C to Farenheit is ${result}°F`);
-                 }
-                 await convertToFarenheit();
+                const temp = Number(await input.question("Convert to Farenheit: "));
+
+                try{
+                    const result = await toFarenheit(temp);
+                    console.log(`The result is ${result}`);
+
+                }catch(error){
+                    console.log(error)
+
+                }
+
             } else {
                 console.log("invalid option")
             }
