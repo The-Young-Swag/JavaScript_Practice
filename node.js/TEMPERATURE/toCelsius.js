b6968c23542ceffebe92af65cb173be2d4a660d9
the-young-swag <xplizit.nm7@gmail.com>
2026-07-21 22:56:12 +0800

    added Celsius converter function

---
+++ node.js/TEMPERATURE/toCelsius.js
@@ -0,0 +1,14 @@
+function toCelsius(temp){
+
+    return new Promise((resolve, reject) =>{
+        setTimeout(() =>{
+            const temp = Number(await input.question("Convert to Celsius: "));
+            const result = (temp - 32) * (5/9);
+        },500);
+    });
+
+}
+
+toCelsius();
+
+module.exports = toCelsius;
