ef98b4d56b013459aff3b5f7340ea6e8df1fdf0f
the-young-swag <xplizit.nm7@gmail.com>
2026-07-21 22:44:27 +0800

    completed index.js

--- node.js/TEMPERATURE/index.js
+++ node.js/TEMPERATURE/index.js
@@ -5,5 +5,38 @@ const input = readline.createInterface({
     output: process.stdout
 });
 
+async function main(){
+    try{
 
+        while(true){
+            const tempSelect = await input.question("\nSelect 1 to convert F to C\nSelect 2 to convert C to F: ");
+
+            if (tempSelect === "1"){
+                const convertToCelsius = async() => {
+                   const temp = Number(await input.question("Convert to Celsius: "));
+                   const result = (temp - 32) * (5/9);
+                   console.log(`${temp}°F to Celsius is ${result}°C`);
+                   
+                }
+                await convertToCelsius();
+
+            } else if (tempSelect === "2"){
+                const convertToFarenheit = async() => {
+                    const temp = Number(await input.question("Convert to Farenheit: "));
+                    const result = (temp * 9/5) + 32;
+                    console.log(`${temp}°C to Farenheit is ${result}°F`);
+                 }
+                 await convertToFarenheit();
+            } else {
+                console.log("invalid option")
+            }
+ 
+        }
+        
+    } finally{
+        input.close();
+    }
+}
+
+main();
 
