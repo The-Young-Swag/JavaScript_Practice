92fc2777a440b9354d9d25c730f82b266fc3836d
the-young-swag <xplizit.nm7@gmail.com>
2026-07-23 22:03:41 +0800

    finished authController

--- banking_system/Controllers/authController.js
+++ banking_system/Controllers/authController.js
@@ -1,2 +1,35 @@
 const accounts = require("./accounts");
 
+function logIn (username, pin) {
+
+    return new Promise((resolve, reject) =>{
+
+        setTimeout( () => {
+            let account;
+
+            for(let i = 0; i < accounts.length; i++){
+                if (accounts[i].username === username &&
+                    accounts[i].pin === pin
+                ){
+                    account = accounts[i];
+                    break
+                }
+
+            }
+
+            if (account){
+
+                resolve({message: `Welcome ${username}`, 
+                        id: account.id, 
+                        name: account.name });
+            
+            }else {
+                reject("Log In Failed")
+            }
+
+        },500)
+
+    });
+
+
+}
