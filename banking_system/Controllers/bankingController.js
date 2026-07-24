cd3b13da28a4c660dd174d8a8d7b57f1a929daed
the-young-swag <xplizit.nm7@gmail.com>
2026-07-24 23:52:20 +0800

    skeleton: code the rest later

---
+++ banking_system/Controllers/bankingController.js
@@ -0,0 +1,29 @@
+const bankingService = require("../services/bankingService");
+
+async function deposit() {
+
+}
+
+async function withdraw() {
+
+}
+
+async function transfer() {
+
+}
+
+async function checkBalance() {
+
+}
+
+async function transactionHistory() {
+
+}
+
+module.exports = {
+    deposit,
+    withdraw,
+    transfer,
+    checkBalance,
+    transactionHistory
+};
